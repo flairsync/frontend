d82f1380313200a700616cb9f99aae48c90e3f82
Semah Chriha <62266399+SemehC@users.noreply.github.com>
2025-09-22 20:19:42 +0200

    FINAL TEST FIX VERCEL

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
+import vercel from "vite-plugin-vercel";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vike(), react(), tailwindcss()],
+  plugins: [vike(), react(), tailwindcss(), vercel()],
 
   build: {
     target: "es2022",
