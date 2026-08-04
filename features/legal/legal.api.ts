e84b277693335d76389f8ac61ba2b6f69c79aaa2
Semah Chriha <62266399+SemehC@users.noreply.github.com>
2026-08-04 12:23:29 +0200

    Fiexed api endpoint

--- features/legal/legal.api.ts
+++ features/legal/legal.api.ts
@@ -2,6 +2,8 @@ import flairapi from "@/lib/flairapi";
 import { unwrap } from "../shared/api-response";
 import { LegalDocument, LegalDocumentType } from "@/models/LegalDocument";
 
+const baseUrl = `${'https://api.flairsync.com/api/v1'}/legal`;
+
 export const fetchCurrentLegalDocumentApiCall = async (
     type: LegalDocumentType
-): Promise<LegalDocument> => unwrap(await flairapi.get(`/legal/${type}/current`));
+): Promise<LegalDocument> => unwrap(await flairapi.get(`${baseUrl}/${type}/current`));
